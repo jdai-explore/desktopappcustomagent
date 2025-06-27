04d1893da29e4db26c2f29b8765ea80c74c83062
jdai-explore <jdaiexplore@gmail.com>
2025-06-27 09:53:23 +0200

    setup issue

--- desktop-agent/src/App.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import AppInitializer from './components/AppInitializer';
-import './App.css';
-
-function App() {
-  return <AppInitializer />;
-}
-
-export default App;
